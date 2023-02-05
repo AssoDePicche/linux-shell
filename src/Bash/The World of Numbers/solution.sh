e9b393e5932aa59b4098042f2e2be397c88882f8
AssoDePicche <samuelprado730@gmail.com>
2023-02-05 13:10:21 -0300

    Feat: Add The World of Numbers

---
+++ src/Bash/The World of Numbers/solution.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Given two integers, X and Y, find their sum, difference, product, and quotient.
+
+read x
+
+read y
+
+echo $[x + y]
+
+echo $[x - y]
+
+echo $[x * y]
+
+echo $[x / y]
